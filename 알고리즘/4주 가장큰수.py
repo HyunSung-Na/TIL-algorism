348891245c65430bc5bc3c439ea10783b12c14c6
HyunSung-Na <59365947+HyunSung-Na@users.noreply.github.com>
2020-07-11 20:29:14 +0900

    알고리즘 물제풀이

---
+++ 알고리즘/4주 가장큰수.py
@@ -0,0 +1,7 @@
+def combination(numbers):
+    numbers = list(map(str, numbers))
+    numbers = sorted(numbers, key=lambda x : (x[0], x[1%len(x)], x[2%len(x)], x[3%len(x)]), reverse=True)
+    return "".join(numbers)
+def solution(numbers):
+    combine = combination(numbers)
+    return combine if int(combine) != 0 else "0"
