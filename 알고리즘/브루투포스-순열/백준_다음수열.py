3b5ecdc072eaedd81251c5ad7e263854681bbea3
HyunSung-Na <59365947+HyunSung-Na@users.noreply.github.com>
2020-10-20 10:36:59 +0900

    브루트포스 기초 문제 풀이

---
+++ 알고리즘/브루투포스-순열/백준_다음수열.py
@@ -0,0 +1,26 @@
+n = int(input())
+search = list(map(int, input().split()))
+nums = []
+
+k = -1
+
+for i in range(len(search) - 1):
+    if search[i] < search[i + 1]:
+        k = i
+
+if k == -1:
+    print(-1)
+else:
+    for j in range(k + 1, len(search)):
+        if search[k] < search[j]:
+            m = j
+
+    search[k], search[m] = search[m], search[k]
+
+    temp = search[k+1:]
+    temp.sort()
+    answer = search[:k+1] + temp
+
+    for num in answer:
+        print(num, end=' ')
+    print()
